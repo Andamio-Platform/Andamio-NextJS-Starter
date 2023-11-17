baecd2eaa5a6e7294a744639455527376d4f292e
nelson-ksh <nelsonksh2021@gmail.com>
2023-11-17 21:39:11 +0530

    andamiojs version update

--- src/andamio/config/index.ts
+++ src/andamio/config/index.ts
@@ -1,40 +1,48 @@
-import { AndamioConfig } from '@andamiojs/core';
+import { AndamioConfig, CourseManagementConfig, ProjectManagementConfig } from "@andamiojs/core";
 
 // Project Management
-import { contributorMinter, contributorReference } from './contributor';
-import { escrows } from './escrow';
-import { treasury } from './treasury';
+import { contributorMinter, contributorReference } from "./contributor";
+import { escrows } from "./escrow";
+import { treasury } from "./treasury";
 
 // Course Management
-import { assignment } from './assignment';
-import { courseReference } from './courseReference';
-import { learnerReference } from './learnerReference';
-import { learnerMintingReference } from './learnerMintingReference';
-import { moduleMintingReference } from './moduleMintingReference';
+import { assignment } from "./assignment";
+import { courseReference } from "./courseReference";
+import { learnerReference } from "./learnerReference";
+import { learnerMintingReference } from "./learnerMintingReference";
+import { moduleMintingReference } from "./moduleMintingReference";
 
-// All Tokens
-import tokens from './tokens.json';
+// Tokens
+import courseManagementTokens from './courseManagementTokens.json'
+import projectManagementTokens from './projectManagementTokens.json'
 
-export const andamioConfig: AndamioConfig = {
-  title: 'Andamio PBL',
-  baseAddress: '',
-  enterpriseAddress: '',
-  rewardAddress: '',
+export const andamioConfig: AndamioConfig<CourseManagementConfig & ProjectManagementConfig> = {
+  title: "Andamio PBL",
+  baseAddress: "",
+  enterpriseAddress: "",
+  rewardAddress: "",
   // at the moment, each contract has a different reference UTxO Address - see individual contract configs
   // referenceScriptAddress: '',
-  metadataKey: '',
-  network: '0',
-  tokens: tokens,
-  treasury: treasury,
-  escrows: escrows,
-  contributorReference: contributorReference,
-  contributorMinter: contributorMinter,
-  courseReference: courseReference,
-  moduleMintingReference: moduleMintingReference,
-  learnerMintingReference: learnerMintingReference,
-  learnerReference: learnerReference,
-  assignment: assignment,
-  nftArtwork: {
-    learnerNFTURL: "https://www.andamio.io/andamio.png",
-  }
+  metadataKey: "",
+  network: "0",
+  config: {
+    assignment: assignment,
+    courseReference: courseReference,
+    learnerReference: learnerReference,
+    learnerMintingReference: learnerMintingReference,
+    moduleMintingReference: moduleMintingReference,
+    courseManagementTokens: courseManagementTokens,
+    coursemanagementNftArtwork: {
+      // move to a file?
+      learnerNFTURL: "https://www.andamio.io/andamio.png",
+      courseModuleNFTURL: "https://www.andamio.io/andamio.png",
+      courseCreatorNFTURL: "https://www.andamio.io/andamio.png",
+      courseDeciderNFTURL: "https://www.andamio.io/andamio.png",
+    },
+    contributorMinter: contributorMinter,
+    contributorReference: contributorReference,
+    escrows: escrows,
+    treasury: treasury,
+    projectManagementTokens: projectManagementTokens,
+  },
 };
