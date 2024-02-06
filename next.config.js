9e62237b21ae5107751b0d114f0043f0c0e2b59a
jamesdunseith <jdunseith@gimbalabs.io>
2024-02-06 06:37:24 -0500

    Revert next.config

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const withMarkdoc = require("@markdoc/next.js");
 module.exports = withMarkdoc(/* options */)({
   pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"],
   reactStrictMode: true,
-  webpack: function (config, options) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
   transpilePackages: ["andamiojs"],
   async redirects() {
     return [
