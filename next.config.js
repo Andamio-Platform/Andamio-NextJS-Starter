ffa282ff62b7b8e03d79d4c0bbaf921e85c3f21e
jamesdunseith <jdunseith@gimbalabs.io>
2024-02-06 06:31:13 -0500

    Content updates

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withMarkdoc = require("@markdoc/next.js");
 module.exports = withMarkdoc(/* options */)({
   pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"],
   reactStrictMode: true,
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
   transpilePackages: ["andamiojs"],
   async redirects() {
     return [
